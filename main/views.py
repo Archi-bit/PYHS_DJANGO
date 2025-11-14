ad33fb8e380e36982fc9c6815f7f102792fda78c
Archi-bit <argennnmmmtv@gmail.com>
2025-11-14 16:31:05 +0600

    Pyhs-Django #1

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница магазина - Home',
+        'list': ['first', 'second'],
+        'dict': {'auto': 21},
+        'is_user': False
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse('About page')
